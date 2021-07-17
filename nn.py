c81e55932c19113bbc0cdde7953bb54fa63e83d9
heidar <heydar.fahmi@sokhan.ai>
2021-07-17 15:23:51 +0430

    modify making network

--- nn.py
+++ nn.py
@@ -4,18 +4,29 @@ import numpy as np
 class NeuralNetwork():
 
     def __init__(self, layer_sizes):
-
-        # TODO
         # layer_sizes example: [4, 10, 2]
-        pass
-
-    def activation(self, x):
-        
+        input_layer_size = layer_sizes[0]
+        hidden_layer_size = layer_sizes[1]
+        output_layer_size = layer_sizes[2]
+        self.activations = [None]
+        self.z = [None, None, None]
+        self.weights = [None]
+        self.bias = [None]
+        self.set_layer(hidden_layer_size, input_layer_size)
+        self.set_layer(output_layer_size, hidden_layer_size)
         # TODO
-        return x
+
+    def set_layer(self, layer_size, last_layer_size):
+        self.activations.append(np.zeros((layer_size, 1)))
+        self.bias.append(np.zeros((layer_size, 1)))
+        self.weights.append(np.random.randn(layer_size, last_layer_size))
+
+    def activation(self, x):  # x is layer index
+        self.z[x] = self.weights[x] @ self.activations[x - 1]
+        self.z[x] = self.z[x] + self.bias[x]
+        self.activations[x] = 1 / (1 + np.exp(-self.z[x]))
 
     def forward(self, x):
-        
-        # TODO
-        # x example: np.array([[0.1], [0.2], [0.3]])
-        pass
+        self.activation[0] = x
+        for layer_index in range(1, 3, 1):
+            self.activation(layer_index)
